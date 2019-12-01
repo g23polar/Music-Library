ff41a1ea7b976771736334dfc80c2daff20ed279
NikolaiLong <57877365+NikolaiLong@users.noreply.github.com>
2019-11-30 19:16:35 -0500

    Update Reader.java

--- src/prj5/Reader.java
+++ src/prj5/Reader.java
@@ -1,8 +1,8 @@
 /**
  * Virginia Tech Honor Code Pledge:
- * As a Hokie, I will conduct myself 
+ * As a Hokie, I will conduct myself
  * with honor and integrity at all times.
- * I will not lie, cheat, or steal, nor 
+ * I will not lie, cheat, or steal, nor
  * will I accept the actions of those who do.
  * Nikolai Long (nikolai)
  */
@@ -16,177 +16,228 @@ import java.util.Scanner;
  * @author Nikolai Long (nikolai)
  * @version Nov 17, 2019
  *
- * Reader Class
+ *          Reader Class
  */
 public class Reader
 {
-    private DLinkedSongs songs;
-    
+    private DLinkedSongs<Song> songs;
+
+
     /**
      * Reader Constructor
-     * @param songFile data file
+     * 
+     * @param songFile
+     *            data file
+     * @throws FileNotFoundException
      */
     public Reader(String songFile, String surveyFile)
+        throws FileNotFoundException
     {
         songs = readSongs(songFile);
         readSurvey(surveyFile);
         Solver solv = new Solver(songs);
+        GUIMusicWindow win = new GUIMusicWindow(solv);
     }
-    
+
+
     /**
      * readSongs Method that creates a list of the
      * songs in the file
-     * @param songFile data file
+     * 
+     * @param songFile
+     *            data file
      * @return songs DLL
-     * @throws FileNotFoundException 
+     * @throws FileNotFoundException
      */
-    public DLinkedSongs readSongs(String songFile) throws FileNotFoundException
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    private DLinkedSongs readSongs(String songFile) throws FileNotFoundException
     {
         DLinkedSongs songs = new DLinkedSongs();
         File file = new File(songFile);
+        @SuppressWarnings("resource")
         Scanner scan = new Scanner(file);
         String[] strings = {};
         if (scan.hasNext())
         {
-            scan.hasNextLine();
+            scan.nextLine();
         }
         while (scan.hasNext())
         {
             strings = scan.nextLine().split(",");
-            songs.add(new Song(strings[0], strings[1], 
-                strings[2], strings[3]));
+            Song newSong = new Song(strings[0], strings[1], strings[2],
+                strings[3]);
+            songs.add(newSong);
+            
         }
         return songs;
     }
-    
+
+
     /**
      * readSurvey Method that uploads the survey
      * data into the songs DLL
+     * 
      * @param surveyFile
+     * @throws FileNotFoundException
      */
-    public void readSurvey(String surveyFile)
+    private void readSurvey(String surveyFile) throws FileNotFoundException
     {
         if (songs == null)
         {
             throw new IllegalStateException();
         }
         File file = new File(surveyFile);
+        @SuppressWarnings("resource")
         Scanner scan = new Scanner(file);
-        String[] strings = {};
+        String[] strings =
+        {};
         if (scan.hasNext())
         {
-            scan.hasNextLine();
+            scan.nextLine();
         }
-        int song = 0;
         while (scan.hasNext())
         {
             strings = scan.nextLine().split(",");
             int[] categories = categories(strings);
-            for (int count = 5; count < strings.length; 
-                count = count + 2)
+            if (categories != null)
             {
-                if (strings[count].equals("No"))
+                int song = 0;
+                for (int count = 5; count < strings.length; count = count + 2)
                 {
-                    // nothing is recorded
-                }
-                else if (strings[count + 1].equals("No"))
-                {
-                    // nothing is recorded
-                    
-                }
-                if (strings[count].equals("Yes"))
-                {
-                    if (strings[count + 1].equals("Yes"))
+                    if (strings[count].equals("Yes") && strings.length > count + 1)
                     {
-                        // recorded as like
-                        songs[song].addLike(categories[0],
-                            categories[1], categories[2]);
+                        if (strings[count + 1].equals("Yes"))
+                        {
+                            // recorded as like
+                            Song toAddto = (Song) songs.getData(song);
+                            toAddto.addLike(categories[0], categories[1],
+                                categories[2]);
+                        }
+                        else
+                        {
+                            // recorded as heard
+                            Song toAddto = (Song) songs.getData(song);
+                            toAddto.addHear(categories[0], categories[1],
+                                categories[2]);
+                        }
                     }
-                    else
-                    {
-                        // recorded as heard
-                        songs[song].addHear(categories[0],
-                            categories[1], categories[2]);
-                    }
+                    // else nothing is recorded
+                    song++;
                 }
-                // else nothing is recorded    
-                song++;
             }
         }
+        Song check = (Song) songs.getData(0);
     }
-    
+
+    /**
+     * categories helper private method
+     * @param strings the line of code to be used
+     * @return the int array of they type of person this is
+     */
     private int[] categories(String[] strings)
     {
         int[] re = new int[3];
-        if (strings[2].equals("Computer Science"))
+        switch (strings[2])
         {
-            re[0] = 0;
+            case "Computer Science":
+                re[0] = 0;
+                break;
+            case "Other Engineering":
+                re[0] = 1;
+                break;
+            case "Math or CMDA":
+                re[0] = 2;
+                break;
+            case "Other":
+                re[0] = 3;
+                break;
+            default:
+                return null;
         }
-        else if (strings[2].equals("Other Engineering"))
+        switch (strings[3])
         {
-            re[0] = 1;
+            case "Northeast":
+                re[1] = 0;
+                break;
+            case "Southeast":
+                re[1] = 1;
+                break;
+            case "United States (other than Southeast or Northwest)":
+                re[1] = 2;
+                break;
+            case "Outside of United States":
+                re[1] = 3;
+                break;
+            default:
+                return null;
         }
-        else if (strings[2].equals("Math or CMDA"))
+        switch (strings[4])
         {
-            re[0] = 2;
-        }
-        else if (strings[2].equals("Other"))
-        {
-            re[0] = 3;
-        }
-        else
-        {
-            throw new IllegalArgumentException();
-        }
-        if (strings[3].equals("Northeast"))
-        {
-            re[1] = 0;
-        }
-        else if (strings[3].equals("Southeast"))
-        {
-            re[1] = 1;
-        }
-        else if (strings[3].equals("United States (other than " +
-            "Southeast or Northwest"))
-        {
-            re[1] = 2;
-        }
-        else if (strings[3].equals("Outside of United States"))
-        {
-            re[1] = 3;
-        }
-        else
-        {
-            throw new IllegalArgumentException();
-        }
-        if (strings[4].equals("reading"))
-        {
-            re[2] = 0;
-        }
-        else if (strings[4].equals("art"))
-        {
-            re[2] = 1;
-        }
-        else if (strings[4].equals("sports"))
-        {
-            re[2] = 2;
-        }
-        else if (strings[4].equals("music"))
-        {
-            re[2] = 3;
-        }
-        else
-        {
-            throw new IllegalArgumentException();
+            case "reading":
+                re[2] = 0;
+                break;
+            case "art":
+                re[2] = 1;
+                break;
+            case "sports":
+                re[2] = 2;
+                break;
+            case "music":
+                re[2] = 3;
+                break;
+            default:
+                return null;
         }
         return re;
     }
     
-    public String toString()
+    public DLinkedSongs<Song> getSongs()
     {
-        StringBuilder build = new StringBuilder("[{");
-        
-        build.append("]");
-        return build.toString();
+        return songs;
+    }
+     
+    /**
+     * to array method for toStringMajor
+     * @return a string array of the songs
+     */
+    public String[] toArrayMajor()
+    {
+        int length = songs.size();
+        String[] re = new String[length];
+        for (int i = 0; i < length; i++)
+        {
+            re[i] = ((Song)songs.getData(i)).toStringMajor();
+        }
+        return re;
+    }
+    
+    /**
+     * to array method for toStringRegion
+     * @return a string array of the songs
+     */
+    public String[] toArrayRegion()
+    {
+        int length = songs.size();
+        String[] re = new String[length];
+        for (int i = 0; i < length; i++)
+        {
+            re[i] = ((Song)songs.getData(i)).toStringRegion();
+        }
+        return re;
+    }
+    
+    /**
+     * to array method for toStringHobby
+     * @return a string array of the songs
+     */
+    public String[] toArrayHobby()
+    {
+        int length = songs.size();
+        String[] re = new String[length];
+        for (int i = 0; i < length; i++)
+        {
+            re[i] = ((Song)songs.getData(i)).toStringHobby();
+        }
+        return re;
     }
 }
