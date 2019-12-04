366817273ece37703cd9eeb6983139403a2b0eb5
g23polar <51683463+g23polar@users.noreply.github.com>
2019-12-03 21:54:42 -0500

    Update Position.java

--- src/prj5/Position.java
+++ src/prj5/Position.java
@@ -1,6 +1,14 @@
 package prj5;
+/**
+ * Virginia Tech Honor Code Pledge:
+ * As a Hokie, I will conduct myself 
+ * with honor and integrity at all times.
+ * I will not lie, cheat, or steal, nor 
+ * will I accept the actions of those who do.
+ * Gautam Nair (g23)
+ */
+public enum GUIPositionEnum {
 
-public enum Position {
-
-    topRight, midRight, bottomRight, topLeft, midLeft, bottomLeft, topCenter, midCenter, bottomCenter;
+    topLeft, topCenter, topRight, midLeft, midCenter, midRight, bottomLeft, bottomCenter, bottomRight; 
+    
 }
